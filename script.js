c2c76192c630230d3c75b34c8d4157ba1cff1f8c
ensiaja <ekikensi@gmail.com>
2026-03-16 02:40:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+body{margin:0;
+font-family:sans-serif;
+text-align:center;}
+
+#cover{height:100vh;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+background:#f5dede;}
+
+#main{display:none;
+padding:40px;}
+
+button{padding:15px 30px;
+font-size:18px;
+border:none;
+background:#c97f7f;
+color:white;
+border-radius:10px;
+cursor:pointer;}
