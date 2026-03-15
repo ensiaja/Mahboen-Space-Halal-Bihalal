80727516fb60fb36f1238b5d0a770e597861ee1d
ensiaja <ekikensi@gmail.com>
2026-03-16 02:48:57 +0700

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-body{margin:0;
+body{
+  
+  margin:0;
 font-family:sans-serif;
-text-align:center;}
+text-align:center;
 
-#cover{height:100vh;
+}
+
+#cover{
+  
+  height:100vh;
 display:flex;
 flex-direction:column;
 justify-content:center;
 align-items:center;
-background:#f5dede;}
+background:#f5dede;
 
-#main{display:none;
-padding:40px;}
+}
 
-button{padding:15px 30px;
+#main{
+  
+display:none;
+padding:40px;
+
+}
+
+button{
+  
+  padding:15px 30px;
 font-size:18px;
 border:none;
 background:#c97f7f;
 color:white;
 border-radius:10px;
-cursor:pointer;}
+cursor:pointer;
+
+}
